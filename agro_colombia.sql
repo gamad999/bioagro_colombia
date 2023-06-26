0b9d56b3d36b8e30f1a168eb206d781d74698250
geovanny999 <gamard199@gmail.com>
2023-06-25 19:21:22 -0500

    Ajuste

--- agro_colombia.sql
+++ agro_colombia.sql
@@ -2,6 +2,8 @@
 
 -- Creación de extension PostGIS en la base de datos Agro Colombia
 
+CREATE EXTENSION postgis;
+
 --- Calculo de areas de Municipios en hectareas -----
 
 ALTER TABLE municipios_valle ADD COLUMN area_has double precision;
@@ -19,25 +21,4 @@ UPDATE municipios_valle SET zona = 'Sur' WHERE mpio_cnmbr = 'PALMIRA'
 ALTER TABLE ecosistema_suelos_resumido ADD COLUMN area_has double precision;
 
 UPDATE ecosistema_suelos_resumido SET area_has = (ST_Area(ecosistema_suelos_resumido.geom) / 10000);
-
-
---- Enlace espacial entre capas de municipios y municipios clase para migrar nombres de entidades territoriales 
---- a capa de municipios clase
-
-ALTER TABLE municipios_clase_valle ADD COLUMN nombre_munic varchar(80);
-
-UPDATE municipios_clase_valle SET nombre_munic = municipios_valle.mpio_cnmbr FROM municipios_valle
-WHERE ST_Intersects(municipios_clase_valle.geom, municipios_valle.geom);
-
---- Geoprocesamiento de capa de ecosistema suelos del Valle del Cauca ---
-
---- Calculo de areas en hectareas ------
-
-ALTER TABLE ecosistema_suelos_valle ADD COLUMN area_has double precision;
-
-UPDATE ecosistema_suelos_valle SET area_has = (SELECT ST_Area(ecosistema_suelos_valle.geom)/10000.0);
-								  
-
-
-
 
