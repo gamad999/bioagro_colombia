575d51c6a1b84d3fa8ffebcaab855bcffaafdfed
geovanny999 <gamard199@gmail.com>
2023-07-04 18:30:12 -0500

    Construcción de tabla de poblacion rural por Municipio

--- social.sql
+++ social.sql
@@ -13,6 +13,11 @@ ALTER TABLE seccion_rural ADD COLUMN municipio varchar(40);
 UPDATE seccion_rural SET municipio = municipios_valle.mpio_cnmbr FROM municipios_valle
 WHERE ST_Intersects(seccion_rural.geom, municipios_valle.geom);
 
+--- Construccion de tabla de población rural por Municipio de acuerdo a datos de secciones rurales Censo 2018 ---
+
+SELECT municipio, SUM(stp27_pers) AS pob_rural FROM seccion_rural
+GROUP BY municipio ORDER BY pob_rural DESC;
+
 
 
 
