7af4ac078b95b7a8bcb9bcf41cba660dcd58ac88
geovanny999 <gamard199@gmail.com>
2023-07-08 20:08:09 -0500

    Construccion de tabla de especies de oquideas en el Valle del Cauca segun base de datos del GBIF

--- agronomico.sql
+++ agronomico.sql
@@ -4,3 +4,9 @@
 
 SELECT genus AS genero, COUNT(DISTINCT species) AS especies, COUNT(DISTINCT id) AS registros
 FROM orquideas_valle GROUP BY genero ORDER BY especies DESC, registros;
+
+-- Construccion de tabla de especies de orquideas presentes en el Valle del Cauca segun GBIF
+
+SELECT species AS especies, COUNT(DISTINCT id) AS registros, MIN(year) AS min_year,
+MAX(year) AS max_year
+FROM orquideas_valle GROUP BY especies ORDER BY registros DESC, min_year, max_year;
