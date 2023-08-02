d6d04a496065ec20e76abe48cb4ddca82f52531c
geovanny999 <gamard199@gmail.com>
2023-08-01 23:27:54 -0500

    Ajuste de nombres de variables de a capa de secciones rurales - creación de campos

--- social.sql
+++ social.sql
@@ -29,3 +29,18 @@ GROUP BY municipio ORDER BY HogJfMujr DESC;
 SELECT municipio, SUM(persconlim) AS PersLimit FROM seccion_rural
 GROUP BY municipio ORDER BY PersLimit DESC;
 
+--- Ajuste de nombres de variables de la capa de secciones rurales para facilitar 
+--- su interpretación por parte de los usuarios ----
+
+--- Creación de campos con nombres estandar de facil interpretación ---
+
+ALTER TABLE seccion_rural ADD COLUMN viviendas int, ADD COLUMN hogaress int,
+ADD COLUMN pob_total int, ADD COLUMN pob_homb int, ADD COLUMN pob_mujr int,
+ADD COLUMN pb_ed0_9 int, ADD COLUMN pb_ed10_19 int, ADD COLUMN pb_ed20_29 int,
+ADD COLUMN pb_ed30_39 int, ADD COLUMN pb_ed40_49 int, ADD COLUMN pb_ed50_59 int,
+ADD COLUMN pb_ed60_69 int, ADD COLUMN pb_ed70_79 int, ADD COLUMN pb_ed80 int, 
+ADD COLUMN nv_prim int, ADD COLUMN nv_secund int, ADD COLUMN nv_profes int,
+ADD COLUMN nv_postgrd int, ADD COLUMN nv_ninguno int, ADD COLUMN undmx_agro int,
+ADD COLUMN unid_agro int, ADD COLUMN vvsn_acue int, ADD COLUMN vvsn_alct int,
+ADD COLUMN vvsn_gas int, ADD COLUMN vvsn_rcbas int, ADD COLUMN vvsn_inter int;
+
