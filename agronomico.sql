8d13b8acbc7eee41304d653afc5f114087afce41
geovanny999 <gamard199@gmail.com>
2023-08-08 17:02:06 -0500

    Construccion de indicadores de riqueza de especies de orquideas por ecosistemas del Valle

--- agronomico.sql
+++ agronomico.sql
@@ -10,3 +10,15 @@ FROM orquideas_valle GROUP BY genero ORDER BY especies DESC, registros;
 SELECT species AS especies, COUNT(DISTINCT id) AS registros, MIN(year) AS min_year,
 MAX(year) AS max_year
 FROM orquideas_valle GROUP BY especies ORDER BY registros DESC, min_year, max_year;
+
+
+--- Construccion de indicadores preliminares de riqueza de especies de orquideas por biomas del bosque seco tropical --
+
+SELECT bioma1996, COUNT(DISTINCT species) AS especies, COUNT(DISTINCT orquideas_valle.id) AS records 
+FROM ecosistemas_valle, orquideas_valle WHERE ST_Intersects(ecosistemas_valle.geom, orquideas_valle.geom)
+GROUP BY bioma1996 ORDER BY especies DESC, records;
+
+
+SELECT nom_ecosistema, COUNT(DISTINCT species) AS especies, COUNT(DISTINCT orquideas_valle.id) AS records 
+FROM ecosistemas_valle, orquideas_valle WHERE ST_Intersects(ecosistemas_valle.geom, orquideas_valle.geom)
+GROUP BY nom_ecosistema ORDER BY especies DESC, records;
