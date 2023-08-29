7c5c99ff4874d7c09913447118900dd2f0457a84
geovanny999 <gamard199@gmail.com>
2023-08-28 22:03:57 -0500

    Segmentación por Municipio de los predios rurales reportados por el IGAC

--- catastral.sql
+++ catastral.sql
@@ -5,3 +5,10 @@
 ALTER TABLE catastro_rural_valle ADD COLUMN area_has double precision;
 
 UPDATE catastro_rural_valle SET area_has = (ST_Area(catastro_rural_valle.geom)/10000.0);
+
+-- Segmentación por Municipio de los predios rurales reportados por el IGAC ----
+
+ALTER TABLE catastro_rural_valle ADD COLUMN nomb_municipio varchar(80);
+
+UPDATE catastro_rural_valle SET nomb_municipio = municipios_valle.mpio_cnmbr FROM municipios_valle
+WHERE catastro_rural_valle.codigo_mun = municipios_valle.mpio_ccdgo;
