b1dd257406f8b6859d3b7fbbb8c6b73c69df2c7d
geovanny999 <gamard199@gmail.com>
2023-09-02 21:27:01 -0500

    Creacion de campo de nombre de Municipios en capa seccion rural forestal

--- agronomico.sql
+++ agronomico.sql
@@ -22,3 +22,10 @@ GROUP BY bioma1996 ORDER BY especies DESC, records;
 SELECT nom_ecosistema, COUNT(DISTINCT species) AS especies, COUNT(DISTINCT orquideas_valle.id) AS records 
 FROM ecosistemas_valle, orquideas_valle WHERE ST_Intersects(ecosistemas_valle.geom, orquideas_valle.geom)
 GROUP BY nom_ecosistema ORDER BY especies DESC, records;
+
+--- Inicio desarrollo consulta de biodiversidad forestal Valle del Cauca por secciones rurales ---
+
+ALTER TABLE seccion_rural_forestal ADD COLUMN municipio varchar(80);
+
+UPDATE seccion_rural_forestal SET municipio = seccion_rural.municipio FROM seccion_rural
+WHERE seccion_rural_forestal.secr_ccnct = seccion_rural.secr_ccnct;
