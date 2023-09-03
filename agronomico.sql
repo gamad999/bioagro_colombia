62f23347f69452068d280afcb94e1b69d854795f
geovanny999 <gamard199@gmail.com>
2023-09-02 22:51:38 -0500

    Diagnostico especies forestales protectoras de aguas y riberas en el Valle segun Kun

--- agronomico.sql
+++ agronomico.sql
@@ -29,3 +29,28 @@ ALTER TABLE seccion_rural_forestal ADD COLUMN municipio varchar(80);
 
 UPDATE seccion_rural_forestal SET municipio = seccion_rural.municipio FROM seccion_rural
 WHERE seccion_rural_forestal.secr_ccnct = seccion_rural.secr_ccnct;
+
+--- Riqueza de especies protectoras de aguas y riberas por seccion rural y Municipio---
+
+ALTER TABLE seccion_rural_forestal ADD COLUMN riq_prot_agua int, ADD COLUMN rec_prot_agua int;
+ALTER TABLE municipios_valle ADD COLUMN riq_prot_agua int, ADD COLUMN rec_prot_agua int;
+-- Riqueza de especies ---
+UPDATE seccion_rural_forestal SET riq_prot_agua = (SELECT COUNT(DISTINCT species) FROM especies_protectoras_aguas
+WHERE ST_Intersects(seccion_rural_forestal.geom, especies_protectoras_aguas.geom));
+
+UPDATE municipios_valle SET riq_prot_agua = (SELECT COUNT(DISTINCT species) FROM especies_protectoras_aguas
+WHERE ST_Intersects(municipios_valle.geom, especies_protectoras_aguas.geom));
+-- Numero de registros GBIF ---
+UPDATE seccion_rural_forestal SET rec_prot_agua = (SELECT COUNT(DISTINCT id) FROM especies_protectoras_aguas
+WHERE ST_Intersects(seccion_rural_forestal.geom, especies_protectoras_aguas.geom));
+
+UPDATE municipios_valle SET rec_prot_agua = (SELECT COUNT(DISTINCT id) FROM especies_protectoras_aguas
+WHERE ST_Intersects(municipios_valle.geom, especies_protectoras_aguas.geom));
+--- Tabla de riqueza de especies y registros por Municipio
+SELECT mpio_cnmbr AS municipio, riq_prot_agua AS especies_prot_aguas, rec_prot_agua AS reg_prot_aguas 
+FROM municipios_valle ORDER BY riq_prot_agua DESC;
+--- Tabla especies protectoras de aguas y riberas ---
+SELECT species, COUNT(DISTINCT id) AS reg_gbif FROM especies_protectoras_aguas
+GROUP BY species ORDER BY reg_gbif DESC;
+
+
